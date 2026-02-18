9669024cc8c3074fa14237d701b43f281d856b90
zahoum <aissazahoum6@gmail.com>
2026-02-18 13:15:44 +0000

    new modification

--- app/Http/Controllers/MyController.php
+++ app/Http/Controllers/MyController.php
@@ -4,25 +4,45 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+// class MyController extends Controller
+// {
+//     public function Afficher()
+//     {
+//         if (!session()->has('affiche')) {
+
+//             $N = rand(1, 10);
+
+//             if ($N === 3) {
+//                 $message = "Golden Number";
+//                 session(['affiche' => true]);
+//             } else {
+//                 $message = "Pas encore Trouver ...";
+//             }
+
+//         } else {
+//             $message = "Vous avez déjà trouvé le nombre d'or !";
+//         }
+
+//         return view('dev', ['msg' => $message]);
+//     }
+// }
 class MyController extends Controller
 {
-    public function Afficher()
+    public function Affichier()
     {
-        if (!session()->has('affiche')) {
-
-            $N = rand(1, 10);
-
-            if ($N === 3) {
+        $N = rand(1, 10);
+        
+        if ($N === 9)
+            {
                 $message = "Golden Number";
-                session(['affiche' => true]);
-            } else {
+                $_SESSION['affiche'] = true;
+                if(isset($_SESSION['affiche']) && $_SESSION['affiche'] == true){
+                    $message = "Vous avez déjà trouvé le nombre d'or !";
+                }
+            }else
+            {
                 $message = "Pas encore Trouver ...";
             }
-
-        } else {
-            $message = "Vous avez déjà trouvé le nombre d'or !";
-        }
-
-        return view('dev', ['msg' => $message]);
+            return view('dev',['msg' => $message]);
     }
 }
